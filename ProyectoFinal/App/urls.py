c2f10e70de677e1665fdf7ad1713e142c0db5809
IgnacioGuerrero <Guerrero.ignacio.joaquin@gmail.com>
2022-09-28 14:13:34 -0300

    Bootstrap fallado :C

---
+++ ProyectoFinal/App/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path    
+from App.views import *
+
+urlpatterns = [
+        path("", inicio, name="inicio"),
+
+]
--- ProyectoFinal/App/views.py
+++ ProyectoFinal/App/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from App.forms import * 
+
 # Create your views here.
+def inicio(request):
+    return render (request, "App/inicio.html")
